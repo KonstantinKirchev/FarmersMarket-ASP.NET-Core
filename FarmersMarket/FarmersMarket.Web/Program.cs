3b424f65f3ced258eefa06a1aa0025e59a69fd54
KonstantinKirchev <konstantin1230@yahoo.com>
2022-02-08 21:06:52 +0200

    Added automapper using

--- FarmersMarket/FarmersMarket.Web/Program.cs
+++ FarmersMarket/FarmersMarket.Web/Program.cs
@@ -6,6 +6,7 @@ using FarmersMarket.Web.Infrastructure.Extensions;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using AutoMapper;
 
 var builder = WebApplication.CreateBuilder(args);
 
