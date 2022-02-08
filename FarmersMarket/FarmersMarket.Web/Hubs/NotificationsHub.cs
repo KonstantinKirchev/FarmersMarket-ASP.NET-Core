d8739fb37d57b9084350ec6cf497b352edb2a6e7
KonstantinKirchev <konstantin1230@yahoo.com>
2022-02-08 15:57:14 +0200

    Added signalR notifications

---
+++ FarmersMarket/FarmersMarket.Web/Hubs/NotificationsHub.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace WomenMarket.App.Hubs
+{
+
+    //[HubName("notifications")]
+    public class NotificationsHub : Hub
+    {
+        public void SendNotification(string type, string notification)
+        {
+            //this.Clients.Others.receiveNotification(type, notification);
+        }
+    }
+}
