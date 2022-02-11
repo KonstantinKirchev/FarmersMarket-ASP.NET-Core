37081a0e8a1f0809cafd498807ca2923d49c4197
KonstantinKirchev <konstantin1230@yahoo.com>
2022-02-11 22:49:39 +0200

    Added new migrations

---
+++ FarmersMarket/FarmersMarket.Data/Migrations/20220211195333_AddUnitsPropertyToProducts.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FarmersMarket.Data.Migrations
+{
+    public partial class AddUnitsPropertyToProducts : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Quantity",
+                table: "Products",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(10)",
+                oldMaxLength: 10);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Unit",
+                table: "Products",
+                type: "nvarchar(10)",
+                maxLength: 10,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Unit",
+                table: "Products");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Quantity",
+                table: "Products",
+                type: "nvarchar(10)",
+                maxLength: 10,
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+    }
+}
