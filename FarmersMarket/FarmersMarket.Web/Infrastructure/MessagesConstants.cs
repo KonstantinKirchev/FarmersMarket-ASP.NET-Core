7a567ae920e81172c96d388c67d59ddc785b47ba
KonstantinKirchev <konstantin1230@yahoo.com>
2022-02-11 22:53:44 +0200

    Added messages file

---
+++ FarmersMarket/FarmersMarket.Web/Infrastructure/MessagesConstants.cs
@@ -0,0 +1,17 @@
+﻿namespace FarmersMarket.Web.Infrastructure
+{
+    public static class MessagesConstants
+    {
+        // Success messages
+        public const string CreateNewFarmSuccessMessage = "You have successfully created a new farm.";
+        public const string CreateNewCategorySuccessMessage = "You have successfully created a new category.";
+        public const string CreateNewProductSuccessMessage = "You have successfully created a new product.";
+        public const string AddProductToCartSuccessMessage = "Product added to the shopping cart.";
+        public const string RemoveProductFromCartSuccessMessage = "Product removed from the shopping cart.";
+
+        // Error messages
+
+
+        // Info messages
+    }
+}
