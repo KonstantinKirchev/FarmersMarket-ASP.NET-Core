9cd51e0bbc852847dbe000e2ded4acaebfcda3d7
KonstantinKirchev <konstantin1230@yahoo.com>
2022-02-12 20:51:51 +0200

    Added payment method

---
+++ FarmersMarket/FarmersMarket.Data/Migrations/20220212175229_AddedPaymentTypeToShoppingCart.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FarmersMarket.Data.Migrations
+{
+    public partial class AddedPaymentTypeToShoppingCart : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "PaymentType",
+                table: "ShoppingCarts",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PaymentType",
+                table: "ShoppingCarts");
+        }
+    }
+}
