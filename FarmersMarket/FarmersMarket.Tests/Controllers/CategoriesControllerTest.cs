e97a4861853731a8da9aa8dca927f6ec527abbf8
KonstantinKirchev <konstantin1230@yahoo.com>
2022-02-24 01:36:32 +0200

    All unit tests are done

---
+++ FarmersMarket/FarmersMarket.Tests/Controllers/CategoriesControllerTest.cs
@@ -0,0 +1,255 @@
+﻿namespace FarmersMarket.Tests.Controllers
+{
+    using AutoMapper;
+    using FarmersMarket.Data.UnitOfWork;
+    using FarmersMarket.Models.BindingModels;
+    using FarmersMarket.Models.EntityModels;
+    using FarmersMarket.Models.ViewModels;
+    using FarmersMarket.Services.Implementations;
+    using FarmersMarket.Services.Interfaces;
+    using FarmersMarket.Web.Areas.Admin.Controllers;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.ViewFeatures;
+    using Moq;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Xunit;
+
+    public class CategoriesControllerTest
+    {
+        private CategoriesController _controller;
+        private MockContainer mocks;
+        private Mock<IFarmersMarketData> dbMock;
+        private ICategoriesService _service;
+        private IMapper mapper;
+
+        private void ConfigureMapper()
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<Category, CategoryViewModel>();
+                cfg.CreateMap<CategoryBindingModel, Category>();
+            });
+
+            this.mapper = config.CreateMapper();
+        }
+
+        public void Init()
+        {
+            this.ConfigureMapper();
+
+            this.dbMock = new Mock<IFarmersMarketData>();
+            this.mocks = new MockContainer();
+            this.mocks.PrepareMocks();
+
+            this.dbMock.Setup(c => c.Categories).Returns(this.mocks.CategoryRepositoryMock.Object);
+
+            this._service = new CategoriesService(this.dbMock.Object, this.mapper);
+            this._controller = new CategoriesController(this._service);
+        }
+
+        [Fact]
+        public void Index_ShouldPass()
+        {
+            // Arrange
+            this.Init();
+
+            // Act
+            var result = _controller.Index() as ViewResult;
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.True(string.IsNullOrEmpty(result.ViewName));
+            Assert.IsType<ViewResult>(result);
+        }
+
+        [Fact]
+        public void Index_ShouldPassCorrectModelToTheView()
+        {
+            // Arrange
+            this.Init();
+
+            // Act
+            var result = _controller.Index() as ViewResult;
+            var categories = result.Model as IEnumerable<CategoryViewModel>;
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.NotNull(categories);
+            Assert.Equal(3, categories.Count());
+            Assert.IsType<ViewResult>(result);
+            Assert.IsType<List<CategoryViewModel>>(categories);
+        }
+
+        [Fact]
+        public void Create_ShouldPass()
+        {
+            // Arrange
+            this.Init();
+
+            // Act
+            var result = _controller.Create() as ViewResult;
+
+            // Assert
+            Assert.IsType<ViewResult>(result);
+        }
+
+        [Fact]
+        public void CreateNullModel_ShouldReturnDefaultView()
+        {
+            // Arrange
+            this.Init();
+            CategoryBindingModel model = null;
+
+            // Act
+            var result = _controller.Create(model);
+
+            // Assert
+            Assert.IsType<ViewResult>(result);
+        }
+
+        [Fact]
+        public void CreateValidModel_ShouldRedirectToIndex()
+        {
+            // Arrange
+            this.Init();
+            CategoryBindingModel model = new CategoryBindingModel()
+            {
+                Id = 4,
+                Name = "Chushki"
+            };
+
+            var httpContext = new DefaultHttpContext();
+            var tempData = new TempDataDictionary(httpContext, Mock.Of<ITempDataProvider>());
+            tempData["SuccessMessage"] = "";
+            _controller.TempData = tempData;
+
+            // Act
+            var result = _controller.Create(model) as ActionResult;
+
+            // Assert
+            var redirectToActionResult = Assert.IsType<RedirectToActionResult>(result);
+            Assert.Null(redirectToActionResult.ControllerName);
+            Assert.Equal("Index", redirectToActionResult.ActionName);
+        }
+
+        [Fact]
+        public void EditValidId_ShouldPass()
+        {
+            // Arrange
+            this.Init();
+
+            // Act
+            var result = _controller.Edit(1) as ViewResult;
+
+            // Assert
+            Assert.IsType<ViewResult>(result);
+        }
+
+        [Fact]
+        public void EditInValidId_ShouldReturnNotFound()
+        {
+            // Arrange
+            this.Init();
+
+            // Act
+            var result = _controller.Edit(4) as StatusCodeResult;
+
+            // Assert
+            Assert.Equal("404", result.StatusCode.ToString());
+        }
+
+        [Fact]
+        public void EditNullModel_ShouldReturnDefaultView()
+        {
+            // Arrange
+            this.Init();
+            CategoryBindingModel model = null;
+
+            // Act
+            var result = _controller.Edit(model);
+
+            // Assert
+            Assert.IsType<ViewResult>(result);
+        }
+
+        [Fact]
+        public void EditValidModel_ShouldRedirectToIndex()
+        {
+            // Arrange
+            this.Init();
+
+            // Act
+            CategoryBindingModel model = new CategoryBindingModel()
+            {
+                Id = 3,
+                Name = "Chushki"
+            };
+
+            var result = _controller.Edit(model);
+
+            // Assert
+            var redirectToActionResult = Assert.IsType<RedirectToActionResult>(result);
+            Assert.Null(redirectToActionResult.ControllerName);
+            Assert.Equal("Index", redirectToActionResult.ActionName);
+        }
+
+        [Fact]
+        public void DeleteInValidId_ShouldReturnNotFound()
+        {
+            // Arrange
+            this.Init();
+
+            // Act
+            var result = _controller.Delete(4) as ActionResult;
+
+            // Assert
+            Assert.IsType<NotFoundResult>(result);
+        }
+
+        [Fact]
+        public void DeleteValidId_ShouldRenderDeleteView()
+        {
+            // Arrange
+            this.Init();
+
+            // Act
+            var result = _controller.Delete(2);
+
+            // Assert
+            Assert.IsType<ViewResult>(result);
+        }
+
+        [Fact]
+        public void DeleteValidId_ShouldPassCorrectModelToTheView()
+        {
+            // Arrange
+            this.Init();
+
+            // Act
+            var result = _controller.Delete(2) as ViewResult;
+            var category = result.Model as CategoryViewModel;
+
+            // Assert
+            Assert.IsType<ViewResult>(result);
+            Assert.IsType<CategoryViewModel>(category);
+            Assert.Equal("Vegetables", category.Name);
+        }
+
+        [Fact]
+        public void DeleteValidId_ShouldRedirectToIndex()
+        {
+            // Arrange
+            this.Init();
+
+            // Act
+            var result = _controller.DeleteConfirmed(2);
+
+            // Assert
+            var redirectToActionResult = Assert.IsType<RedirectToActionResult>(result);
+            Assert.Null(redirectToActionResult.ControllerName);
+            Assert.Equal("Index", redirectToActionResult.ActionName);
+        }
+    }
+}
---
+++ FarmersMarket/FarmersMarket.Tests/Services/CategoriesServiceTest.cs
@@ -0,0 +1,175 @@
+﻿namespace FarmersMarket.Tests.Services
+{
+    using AutoMapper;
+    using FarmersMarket.Data;
+    using FarmersMarket.Data.UnitOfWork;
+    using FarmersMarket.Models.BindingModels;
+    using FarmersMarket.Models.EntityModels;
+    using FarmersMarket.Models.ViewModels;
+    using FarmersMarket.Services.Implementations;
+    using FarmersMarket.Services.Interfaces;
+    using FluentAssertions;
+    using Microsoft.EntityFrameworkCore;
+    using Moq;
+    using System;
+    using System.Linq;
+    using Xunit;
+
+    public class CategoriesServiceTest
+    {
+        private MockContainer mocks;
+        private Mock<IFarmersMarketData> dbMock;
+        private ICategoriesService service;
+        private IMapper mapper;
+
+        private void ConfigureMapper()
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<Category, CategoryViewModel>();
+                cfg.CreateMap<CategoryBindingModel, Category>();
+            });
+
+            this.mapper = config.CreateMapper();
+        }
+
+        public void Init()
+        {
+            this.ConfigureMapper();
+
+            this.dbMock = new Mock<IFarmersMarketData>();
+            this.mocks = new MockContainer();
+            this.mocks.PrepareMocks();
+
+            this.dbMock.Setup(c => c.Categories).Returns(this.mocks.CategoryRepositoryMock.Object);
+            this.service = new CategoriesService(this.dbMock.Object, this.mapper);
+        }
+
+        [Fact]
+        public void GetAllCategories_ShouldReturnGivenCategories()
+        {
+            // Arrange
+            this.Init();
+            
+            // Act
+            var categories = this.service.GetAllCategories();
+            
+            // Assert
+            Assert.NotNull(categories);
+            Assert.Equal(3, categories.Count());
+            Assert.Equal("Fruits", categories.FirstOrDefault().Name);
+        }
+
+        [Fact]
+        public void GetEditCategoryWithValidId_ShouldReturnGivenCategory()
+        {
+            // Arrange
+            this.Init();
+
+            // Act
+            var category = this.service.GetEditCategory(1);
+
+            // Assert
+            Assert.NotNull(category);
+            Assert.Equal(1, category.Id);
+            Assert.Equal("Fruits", category.Name);
+        }
+
+        [Fact]
+        public void GetEditCategoryWithInvalidId_ShouldReturnNull()
+        {
+            // Arrange
+            this.Init();
+
+            // Act
+            var category = this.service.GetEditCategory(4);
+
+            // Assert
+            Assert.Null(category);
+        }
+
+        [Fact]
+        public void GetDeleteCategoryWithValidId_ShouldReturnGivenCategory()
+        {
+            // Arrange
+            this.Init();
+
+            // Act
+            var category = this.service.GetDeleteCategory(2);
+
+            // Assert
+            Assert.NotNull(category);
+            Assert.Equal(2, category.Id);
+            Assert.Equal("Vegetables", category.Name);
+        }
+
+        [Fact]
+        public void GetDeleteCategoryWithInvalidId_ShouldReturnNull()
+        {
+            // Arrange
+            this.Init();
+
+            // Act
+            var category = this.service.GetDeleteCategory(4);
+
+            // Assert
+            Assert.Null(category);
+        }
+
+        [Fact]
+        public void PostValidCategory_ShouldAddToRepo()
+        {
+            // Arrange
+            this.Init();
+
+            // Act
+            CategoryBindingModel testCategory = new CategoryBindingModel() { Name = "Meats" };
+            this.service.CreateNewCategory(testCategory);
+
+            // Assert
+            Assert.Equal(4, this.dbMock.Object.Categories.All().Count());
+        }
+
+        [Fact]
+        public void PutValidCategory_ShouldModifyExistingCategory()
+        {
+            // Arrange
+            this.Init();
+
+            // Act
+            CategoryBindingModel testCategory = new CategoryBindingModel() { Id = 1, Name = "Fish" };
+            this.service.EditCategory(testCategory);
+
+            // Assert
+            Assert.Equal("Fish", this.dbMock.Object.Categories.Find(1).Name);
+        }
+
+        [Fact]
+        public void DeleteExistingCategory_ShouldDeleteCategoryFromRepo()
+        {
+            // Arrange
+            this.Init();
+
+            // Act
+            this.service.DeleteCategory(2);
+            var category = this.dbMock.Object.Categories.Find(2);
+
+            // Assert
+            Assert.True(category.IsDeleted);
+            Assert.Equal(3, this.dbMock.Object.Categories.All().Count());
+        }
+
+        [Fact]
+        public void DeleteNotExistingCategory_ShouldNotDeleteCategoryFromRepo()
+        {
+            // Arrange
+            this.Init();
+
+            // Act
+            this.service.DeleteCategory(4);
+
+            // Assert
+            Assert.Equal(3, this.dbMock.Object.Categories.All().Count());
+        }
+    }
+}
