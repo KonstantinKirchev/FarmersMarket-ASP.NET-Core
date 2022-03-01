aa0ee097af85f3a6068405ae6be318f1b2fcc389
KonstantinKirchev <konstantin1230@yahoo.com>
2022-03-01 17:23:42 +0200

    Added new messages

--- FarmersMarket/FarmersMarket.Web/Infrastructure/Constants/MessagesConstants.cs
+++ FarmersMarket/FarmersMarket.Web/Infrastructure/Constants/MessagesConstants.cs
@@ -3,9 +3,15 @@
     public static class MessagesConstants
     {
         // Success messages
-        public const string CreateNewFarmSuccessMessage = "You have successfully created a new farm.";
-        public const string CreateNewCategorySuccessMessage = "You have successfully created a new category.";
-        public const string CreateNewProductSuccessMessage = "You have successfully created a new product.";
+        public const string CreateFarmSuccessMessage = "You have successfully created a farm.";
+        public const string EditFarmSuccessMessage = "You have successfully edited a farm.";
+        public const string DeleteFarmSuccessMessage = "You have successfully deleted a farm.";
+        public const string CreateCategorySuccessMessage = "You have successfully created a category.";
+        public const string EditCategorySuccessMessage = "You have successfully edited a category.";
+        public const string DeleteCategorySuccessMessage = "You have successfully deleted a category.";
+        public const string CreateProductSuccessMessage = "You have successfully created a product.";
+        public const string EditProductSuccessMessage = "You have successfully edited a product.";
+        public const string DeleteProductSuccessMessage = "You have successfully deleted a product.";
         public const string AddProductToCartSuccessMessage = "Product added to the shopping cart.";
         public const string RemoveProductFromCartSuccessMessage = "Product removed from the shopping cart.";
         public const string TheOrderWasSuccessfullyPlaced = "The order was placed successfully";
